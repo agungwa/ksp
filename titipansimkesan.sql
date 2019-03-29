aa4ff0a977e0ec99eaa82ec1c916e034f234f2e2
agungwa <agung.widhiatmojo@gmail.com>
2019-03-29 20:08:22 +0700

    tabel titipansimkesan , CRUD titipansimkesan

---
+++ titipansimkesan.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 29, 2019 at 01:51 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 5.6.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ksp_sidomukti`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `titipansimkesan`
+--
+
+CREATE TABLE `titipansimkesan` (
+  `tts_id` int(11) NOT NULL,
+  `sik_id` int(11) NOT NULL COMMENT 'fk dr simkesan',
+  `tts_tgltitip` datetime NOT NULL,
+  `tts_jmltitip` float NOT NULL,
+  `tts_jmlambil` float NOT NULL,
+  `tts_status` tinyint(1) NOT NULL,
+  `tts_tgl` datetime NOT NULL,
+  `tts_flag` tinyint(2) NOT NULL,
+  `tts_info` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `titipansimkesan`
+--
+ALTER TABLE `titipansimkesan`
+  ADD PRIMARY KEY (`tts_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `titipansimkesan`
+--
+ALTER TABLE `titipansimkesan`
+  MODIFY `tts_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
