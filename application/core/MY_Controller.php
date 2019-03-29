af01df5d75fbe6f3ee8dbcbd403ec25ee9dc6588
Robet Habibi <robeth.steve@gmail.com>
2019-03-29 15:44:55 +0700

    improve core controller

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -4,9 +4,16 @@ if (!defined('BASEPATH'))
 
 class MY_Base extends CI_Controller {
 
+	var $tgl;
+	var $flag;
+
 	function __construct()
     {
         parent::__construct();
+       	$this->tgl = date('Y-m-d H:i:s');
+       	$this->flag = array(0=>"New", 1=>"Updated", 2=>"Deleted");
+        $this->load->vars($this->tgl, $this->flag);
+
         if ($this->session->userdata('logged')!=TRUE){
             redirect(site_url('Auth'));
         }
