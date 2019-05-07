82617d87366f0fc9b9059e1361f62fa716916ff6
agungwa <agung.widhiatmojo@gmail.com>
2019-05-07 18:27:43 +0700

    view form

--- application/views/backend/settingsimpanan/settingsimpanan_form.php
+++ application/views/backend/settingsimpanan/settingsimpanan_form.php
@@ -12,18 +12,18 @@
                 </div>
         
         <form action="<?php echo $action; ?>" method="post">
-        <div class="ibox-content col-md-3">
+        <div class="ibox-content col-md-6">
 	    <div class="form-group col-md-12">
             <label for="varchar">Nama <?php echo form_error('ses_nama') ?></label>
             <input type="text" class="form-control" name="ses_nama" id="ses_nama" placeholder="Nama" value="<?php echo $ses_nama; ?>" />
         </div>
         
-	    <div class="form-group col-md-12">
+	    <div class="form-group col-md-6">
             <label for="float">Minimal<?php echo form_error('ses_min') ?></label>
             <input type="number" class="form-control" name="ses_min" id="ses_min" placeholder="Min" value="<?php echo $ses_min; ?>" />
         </div>
         
-	    <div class="form-group col-md-12">
+	    <div class="form-group col-md-6">
             <label for="float">Maximal<?php echo form_error('ses_max') ?></label>
             <input type="number" class="form-control" name="ses_max" id="ses_max" placeholder="Max" value="<?php echo $ses_max; ?>" />
         </div>
