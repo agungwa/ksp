27363140c6245f28280f5e587c54db53aa7d1c80
agungwa <agung.widhiatmojo@gmail.com>
2019-05-08 10:54:48 +0700

    view

--- application/views/backend/penarikansimpananwajib/penarikansimpananwajib_list.php
+++ application/views/backend/penarikansimpananwajib/penarikansimpananwajib_list.php
@@ -8,7 +8,7 @@
         <div class="col-lg-12">
             <div class="ibox float-e-margins">
                 <div class="ibox-title">
-                    <h2><b>List Penarikansimpananwajib</b></h2>
+                    <h2><b>Penarikan Simpanan Wajib</b></h2>
                     <?php if ($this->session->userdata('message') != '') {?>
                     <div class="alert alert-success alert-dismissable">
                                 <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
@@ -48,9 +48,9 @@
             <thead class="thead-light">
             <tr>
                 <th class="text-center">No</th>
-		<th class="text-center">Siw Id</th>
-		<th class="text-center">Psw Tglpenarikan</th>
-		<th class="text-center">Psw Jumlah</th>
+		<th class="text-center">Simpanan Wajib</th>
+		<th class="text-center">Tanggal Penarikan</th>
+		<th class="text-center">Jumlah</th>
 		<th class="text-center">Action</th>
             </tr>
             </thead>
--- application/views/backend/penarikansimpananwajib/penarikansimpananwajib_read.php
+++ application/views/backend/penarikansimpananwajib/penarikansimpananwajib_read.php
@@ -7,16 +7,16 @@
     <div class="col-lg-12">
     <div class="ibox float-e-margins">
         <div class="ibox-title">
-            <h2 style="margin-top:0px">Penarikansimpananwajib Read</h2>
+            <h2 style="margin-top:0px">Penarikan Simpanan Wajib</h2>
             <div class="ibox-tools">
             </div>
         </div>
         <div class="ibox-content">
         
         <table class="table">
-	    <tr><td>Siw Id</td><td><?php echo $siw_id; ?></td></tr>
-	    <tr><td>Psw Tglpenarikan</td><td><?php echo $psw_tglpenarikan; ?></td></tr>
-	    <tr><td>Psw Jumlah</td><td><?php echo $psw_jumlah; ?></td></tr>
+	    <tr><td>Simpanan Wajib</td><td><?php echo $siw_id; ?></td></tr>
+	    <tr><td>Tanggal Penarikan</td><td><?php echo $psw_tglpenarikan; ?></td></tr>
+	    <tr><td>Jumlah</td><td><?php echo $psw_jumlah; ?></td></tr>
 	    <tr><td></td><td><a href="<?php echo site_url('penarikansimpananwajib') ?>" class="btn btn-default">Cancel</a></td></tr>
 	</table>
             </div>
