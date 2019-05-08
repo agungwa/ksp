1711a86756e4217a78dd19a0f980f6366fbdfdd6
agungwa <agung.widhiatmojo@gmail.com>
2019-05-08 12:33:18 +0700

    view

--- application/views/backend/mutasisimpanan/mutasisimpanan_form.php
+++ application/views/backend/mutasisimpanan/mutasisimpanan_form.php
@@ -8,40 +8,54 @@
         <div class="col-lg-12">
             <div class="ibox float-e-margins">
                 <div class="ibox-title">
-                    <h2 style="margin-top:0px"><?php echo $button ?> Mutasisimpanan </h2>
+                    <h2 style="margin-top:0px"><?php echo $button ?> Mutasi Simpanan </h2>
                 </div>
         
         <form action="<?php echo $action; ?>" method="post">
-        <div class="ibox-content">
+        <div class="ibox-content col-md-12">
+        <div class="col-md-4">
         <div class="form-group">
-            <label for="varchar">Sim Kode <?php echo form_error('sim_kode') ?></label>
+            <label for="varchar">Rekening Simpanan <?php echo form_error('sim_kode') ?></label>
             <div class="input-group">
             <input type="hidden" name="sim_kode" id="sim_kode" value="<?php echo $sim_kode; ?>" />
-            <input type="text" class="form-control" name="nm_sim_kode" id="nm_sim_kode" placeholder="Simpanan" value="<?php echo $nm_sim_kode; ?>" readonly/>
+            <input type="text" class="form-control" name="nm_sim_kode" id="nm_sim_kode" placeholder="Rekening Simpanan" value="<?php echo $nm_sim_kode; ?>" readonly/>
             <div class="input-group-addon">
                 <span onclick="lookup('<?=base_url()?>simpanan/lookup','sim_kode');" style="cursor: pointer;">Cari</span>
             </div>
             </div>
         </div>
 	    <div class="form-group">
-            <label for="datetime">Mus Tglmutasi <?php echo form_error('mus_tglmutasi') ?></label>
-            <input type="text" class="form-control" name="mus_tglmutasi" id="mus_tglmutasi" placeholder="Mus Tglmutasi" value="<?php echo $mus_tglmutasi; ?>" />
+            <label for="datetime">Tanggal Mutasi <?php echo form_error('mus_tglmutasi') ?></label>
+            <input type="date" class="form-control" name="mus_tglmutasi" id="todays-date" placeholder="Tanggal Mutasi" value="<?php echo $mus_tglmutasi; ?>" />
+        </div>
+        <div class="form-group">
+            <label for="varchar">Wilayah Asal <?php echo form_error('mus_asal') ?></label>
+            <div class="input-group">
+            <input type="hidden" name="mus_asal" id="mus_asal" value="<?php echo $mus_asal; ?>" />
+            <input type="text" class="form-control" name="nm_mus_asal" id="nm_mus_asal" placeholder="Wilayah Asal" value="<?php echo $nm_mus_asal; ?>" readonly/>
+            <div class="input-group-addon">
+                <span onclick="lookup('<?=base_url()?>wilayah/lookup','wil_kode');" style="cursor: pointer;">Cari</span>
+            </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="varchar">Wilayah Tujuan <?php echo form_error('mus_tujuan') ?></label>
+            <div class="input-group">
+            <input type="hidden" name="mus_tujuan" id="mus_tujuan" value="<?php echo $mus_tujuan; ?>" />
+            <input type="text" class="form-control" name="nm_mus_tujuan" id="nm_mus_tujuan" placeholder="Wilayah Tujuan" value="<?php echo $nm_mus_tujuan; ?>" readonly/>
+            <div class="input-group-addon">
+                <span onclick="lookup('<?=base_url()?>wilayah/lookup','wil_kode');" style="cursor: pointer;">Cari</span>
+            </div>
+            </div>
         </div>
 	    <div class="form-group">
-            <label for="varchar">Mus Asal <?php echo form_error('mus_asal') ?></label>
-            <input type="text" class="form-control" name="mus_asal" id="mus_asal" placeholder="Mus Asal" value="<?php echo $mus_asal; ?>" />
-        </div>
-	    <div class="form-group">
-            <label for="varchar">Mus Tujuan <?php echo form_error('mus_tujuan') ?></label>
-            <input type="text" class="form-control" name="mus_tujuan" id="mus_tujuan" placeholder="Mus Tujuan" value="<?php echo $mus_tujuan; ?>" />
-        </div>
-	    <div class="form-group">
-            <label for="tinyint">Mus Status <?php echo form_error('mus_status') ?></label>
-            <input type="text" class="form-control" name="mus_status" id="mus_status" placeholder="Mus Status" value="<?php echo $mus_status; ?>" />
+            <label for="tinyint">Status <?php echo form_error('mus_status') ?></label>
+            <input type="number" class="form-control" name="mus_status" id="mus_status" placeholder="Status" value="<?php echo $mus_status; ?>" />
         </div>
 	    <input type="hidden" name="mus_id" value="<?php echo $mus_id; ?>" /> 
 	    <button type="submit" class="btn btn-primary"><?php echo $button ?></button> 
 	    <a href="<?php echo site_url('mutasisimpanan') ?>" class="btn btn-default">Cancel</a>
+        </div>
 	</div>
             </form>
         </div>
