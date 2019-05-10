fbee69efe02574850baa222eaa3ff6a70d8b751b
agungwa <agung.widhiatmojo@gmail.com>
2019-05-10 20:24:57 +0700

    view

--- application/views/backend/penarikaninvestasiberjangka/penarikaninvestasiberjangka_form.php
+++ application/views/backend/penarikaninvestasiberjangka/penarikaninvestasiberjangka_form.php
@@ -8,36 +8,38 @@
         <div class="col-lg-12">
             <div class="ibox float-e-margins">
                 <div class="ibox-title">
-                    <h2 style="margin-top:0px"><?php echo $button ?> Penarikaninvestasiberjangka </h2>
+                    <h2 style="margin-top:0px"><?php echo $button ?> Penarikan Investasi Berjangka </h2>
                 </div>
         
         <form action="<?php echo $action; ?>" method="post">
-        <div class="ibox-content">
+        <div class="ibox-content col-md-12">
+        <div class="col-md-4">
 	    <div class="form-group">
-            <label for="varchar">Investasi Kode <?php echo form_error('ivb_kode') ?></label>
+            <label for="varchar">Rekening Investasi <?php echo form_error('ivb_kode') ?></label>
             <div class="input-group">
             <input type="hidden" name="ivb_kode" id="ivb_kode" value="<?php echo $ivb_kode; ?>" />
-            <input type="text" class="form-control" name="nm_ivb_kode" id="nm_ivb_kode" placeholder="Investasi Kode" value="<?php echo $nm_ivb_kode; ?>" readonly/>
+            <input type="text" class="form-control" name="nm_ivb_kode" id="nm_ivb_kode" placeholder="Rekening Investasi" value="<?php echo $nm_ivb_kode; ?>" readonly/>
             <div class="input-group-addon">
                 <span onclick="lookup('<?=base_url()?>investasiberjangka/lookup','ivb_kode');" style="cursor: pointer;">Cari</span>
             </div>
             </div>
         </div>
 	    <div class="form-group">
-            <label for="tinyint">Pib Penarikanke <?php echo form_error('pib_penarikanke') ?></label>
-            <input type="text" class="form-control" name="pib_penarikanke" id="pib_penarikanke" placeholder="Pib Penarikanke" value="<?php echo $pib_penarikanke; ?>" />
+            <label for="tinyint">Penarikanke <?php echo form_error('pib_penarikanke') ?></label>
+            <input type="text" class="form-control" name="pib_penarikanke" id="pib_penarikanke" placeholder="Penarikanke" value="<?php echo $pib_penarikanke; ?>" />
         </div>
 	    <div class="form-group">
-            <label for="float">Pib Jmlkeuntungan <?php echo form_error('pib_jmlkeuntungan') ?></label>
-            <input type="text" class="form-control" name="pib_jmlkeuntungan" id="pib_jmlkeuntungan" placeholder="Pib Jmlkeuntungan" value="<?php echo $pib_jmlkeuntungan; ?>" />
+            <label for="float">Jumlah Keuntungan <?php echo form_error('pib_jmlkeuntungan') ?></label>
+            <input type="number" class="form-control" name="pib_jmlkeuntungan" id="pib_jmlkeuntungan" placeholder="Jumlah Keuntungan" value="<?php echo $pib_jmlkeuntungan; ?>" />
         </div>
 	    <div class="form-group">
-            <label for="float">Pib Jmlditerima <?php echo form_error('pib_jmlditerima') ?></label>
-            <input type="text" class="form-control" name="pib_jmlditerima" id="pib_jmlditerima" placeholder="Pib Jmlditerima" value="<?php echo $pib_jmlditerima; ?>" />
+            <label for="float">Jumlah Diterima <?php echo form_error('pib_jmlditerima') ?></label>
+            <input type="number" class="form-control" name="pib_jmlditerima" id="pib_jmlditerima" placeholder="Jumlah Diterima" value="<?php echo $pib_jmlditerima; ?>" />
         </div>
 	    <input type="hidden" name="pib_id" value="<?php echo $pib_id; ?>" /> 
 	    <button type="submit" class="btn btn-primary"><?php echo $button ?></button> 
 	    <a href="<?php echo site_url('penarikaninvestasiberjangka') ?>" class="btn btn-default">Cancel</a>
+        </div>
 	</div>
             </form>
         </div>
