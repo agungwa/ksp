6e1a254866efd48e6c2d1f9de6ef72406304f7e8
agungwa <agung.widhiatmojo@gmail.com>
2019-05-11 16:35:08 +0700

    view

--- application/views/backend/settingangsuran/settingangsuran_list.php
+++ application/views/backend/settingangsuran/settingangsuran_list.php
@@ -8,7 +8,7 @@
         <div class="col-lg-12">
             <div class="ibox float-e-margins">
                 <div class="ibox-title">
-                    <h2><b>List Settingangsuran</b></h2>
+                    <h2><b>Setting Angsuran</b></h2>
                     <?php if ($this->session->userdata('message') != '') {?>
                     <div class="alert alert-success alert-dismissable">
                                 <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
@@ -48,8 +48,8 @@
             <thead class="thead-light">
             <tr>
                 <th class="text-center">No</th>
-		<th class="text-center">Sea Tenor</th>
-		<th class="text-center">Sea Tgl</th>
+		<th class="text-center">Tenor</th>
+		<th class="text-center">Tanggal</th>
 		<th class="text-center">Action</th>
             </tr>
             </thead>
--- application/views/backend/settingangsuran/settingangsuran_read.php
+++ application/views/backend/settingangsuran/settingangsuran_read.php
@@ -7,14 +7,14 @@
     <div class="col-lg-12">
     <div class="ibox float-e-margins">
         <div class="ibox-title">
-            <h2 style="margin-top:0px">Settingangsuran Read</h2>
+            <h2 style="margin-top:0px">Setting Angsuran Read</h2>
             <div class="ibox-tools">
             </div>
         </div>
         <div class="ibox-content">
         
         <table class="table">
-	    <tr><td>Sea Tenor</td><td><?php echo $sea_tenor; ?></td></tr>
+	    <tr><td>Tenor</td><td><?php echo $sea_tenor; ?></td></tr>
 	    <tr><td></td><td><a href="<?php echo site_url('settingangsuran') ?>" class="btn btn-default">Cancel</a></td></tr>
 	</table>
             </div>
