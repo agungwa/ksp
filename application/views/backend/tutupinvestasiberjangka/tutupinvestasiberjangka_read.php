5f5bdbd880ba79ecb387cded4abf84c085e3f1e8
agungwa <agung.widhiatmojo@gmail.com>
2019-05-12 21:50:42 +0700

    view

--- application/views/backend/tutupinvestasiberjangka/tutupinvestasiberjangka_read.php
+++ application/views/backend/tutupinvestasiberjangka/tutupinvestasiberjangka_read.php
@@ -14,9 +14,9 @@
         <div class="ibox-content">
         
         <table class="table">
-	    <tr><td>Ivb Kode</td><td><?php echo $ivb_kode; ?></td></tr>
-	    <tr><td>Tib Tgltutup</td><td><?php echo $tib_tgltutup; ?></td></tr>
-	    <tr><td>Tib Catatan</td><td><?php echo $tib_catatan; ?></td></tr>
+	    <tr><td>Rekening Investasi</td><td><?php echo $ivb_kode; ?></td></tr>
+	    <tr><td>Tanggal Tutup</td><td><?php echo $tib_tgltutup; ?></td></tr>
+	    <tr><td>Catatan</td><td><?php echo $tib_catatan; ?></td></tr>
 	    <tr><td></td><td><a href="<?php echo site_url('tutupinvestasiberjangka') ?>" class="btn btn-default">Batal</a></td></tr>
 	</table>
             </div>
