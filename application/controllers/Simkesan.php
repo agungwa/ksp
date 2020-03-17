f1931510ea42ba5da4291261f1c06b356177f72a
Agung Widhiatmojo <agung.widhiatmojo@gmail.com>
2020-03-17 15:30:08 +0700

    disable otomatis hangus

--- application/controllers/Simkesan.php
+++ application/controllers/Simkesan.php
@@ -56,26 +56,26 @@ class Simkesan extends MY_Base
 		
 		foreach($setoransimkesan as $k) {
 			
-			$kode		= $k->sik_kode;
-			$tanggal 	= new DateTime($k->tanggal); 
-			$sekarang 	= new DateTime();
-			$perbedaan 	= $tanggal->diff($sekarang);
+		// 	$kode		= $k->sik_kode;
+		// 	$tanggal 	= new DateTime($k->tanggal); 
+		// 	$sekarang 	= new DateTime();
+		// 	$perbedaan 	= $tanggal->diff($sekarang);
 			
-			$satu=1;
-			$tiga=3;
+		// 	$satu=1;
+		// 	$tiga=3;
 			
-			$tgl_jt			=date("Y-m-d", strtotime($k->ssk_tglsetoran));
-			$tgl_3bln		=date("Y-m-d", strtotime($k->ssk_tglsetoran.' + '.$tiga.'month'.' + '.$satu.'days'));
-			$sekarang		=date("Y-m-d");
+		// 	$tgl_jt			=date("Y-m-d", strtotime($k->ssk_tglsetoran));
+		// 	$tgl_3bln		=date("Y-m-d", strtotime($k->ssk_tglsetoran.' + '.$tiga.'month'.' + '.$satu.'days'));
+		// 	$sekarang		=date("Y-m-d");
 			
-			$data = array('sik_status'=>3);
+		// 	$data = array('sik_status'=>3);
 			
-			if($perbedaan->m > 2){
-				if($sekarang > $tgl_3bln)
-					$this->Simkesan_model->update($kode, $data);
-				//echo $perbedaan->m.'<br>';
-			}
-		}
+		// 	if($perbedaan->m > 2){
+		// 		if($sekarang > $tgl_3bln)
+		// 			$this->Simkesan_model->update($kode, $data);
+		// 		//echo $perbedaan->m.'<br>';
+		// 	}
+		// }
 		
         $nowYear = date('d');
         $data = array(
